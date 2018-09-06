74f8271e468128bd75583f0f62365c449bc8cf4d
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 18:26:45 -0500

    Par 3 finished. CombinedViewController completed.

--- PollTacoCat/View Controller/CombinedViewController.swift
+++ PollTacoCat/View Controller/CombinedViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class CombinedViewController: UIViewController {
+class CombinedViewController: UIViewController, VoteControllerProtocol {
+    
+    var voteController: VoteController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +18,17 @@ class CombinedViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+        if segue.identifier == "VotingSegue" {
+            guard let votingVC = segue.destination as? VoteControllerProtocol else { return }
+            votingVC.voteController = voteController
+            
+        } else if segue.identifier == "ResultsSegue" {
+            guard let resultsVC = segue.destination as? VoteControllerProtocol else { return }
+            resultsVC.voteController = voteController
+        }
+    }
 }
