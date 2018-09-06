a24e9874bb66ce8b2761435ae3ba2332b9b0b3a8
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 18:13:03 -0500

    Part 3. ResultsTableViewController completed.

--- PollTacoCat/View Controller/VotingViewController.swift
+++ PollTacoCat/View Controller/VotingViewController.swift
@@ -27,16 +27,4 @@ class VotingViewController: UIViewController, VoteControllerProtocol {
         
         voteController?.createVote(name: name, response: response)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
