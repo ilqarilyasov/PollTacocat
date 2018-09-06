5b9fec9dad4355761c798a45ed26e5b6d7ef12dc
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 17:25:36 -0500

    Part 3. VoteControllerProtocol created.

---
+++ PollTacoCat/Protocol/VoteControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  VoteControllerProtocol.swift
+//  PollTacoCat
+//
+//  Created by Ilgar Ilyasov on 9/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol VoteControllerProtocol {
+    var voteController: VoteController? { get set }
+}
